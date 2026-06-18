c3075cef41bdc811a6515495a92191f4a847751b
huntbyington <hunter.byb@gmail.com>
2026-06-17 19:28:51 -0600

    Updating data memory header to match documentation

--- src/data_memory.sv
+++ src/data_memory.sv
@@ -7,12 +7,14 @@ module data_memory (
     input wire [`DATA_WIDTH-1:0] i_data,
     input wire [$clog2(`MEM_SIZE)-1:0] i_addr,
     // OUTPUT
-    output logic [`DATA_WIDTH-1:0] o_data
+    output wire [`DATA_WIDTH-1:0] o_data
 );
 
-    logic [`DATA_WIDTH-1:0] memory [`MEM_SIZE / 4];
-    logic [$clog2(`MEM_SIZE)-3:0] shifted_addr;
+    reg [`DATA_WIDTH-1:0] memory [`MEM_SIZE / 4];
+    wire [$clog2(`MEM_SIZE)-3:0] shifted_addr;
+    reg [`DATA_WIDTH-1:0] o_data_reg; // Internal register to hold synchronous output data
 
+    assign o_data = o_data_reg; // Continuous assignment to output the data from the internal register
     assign shifted_addr = i_addr[$clog2(`MEM_SIZE)-1:2]; // Shift right by 2 bits for word addressing
 
     always_ff @(posedge i_clk) begin
@@ -23,10 +25,10 @@ module data_memory (
 
     always_ff @(posedge i_clk) begin
         if (i_we) begin
-            o_data <= i_data; // Output the data being written
+            o_data_reg <= i_data; // Output the data being written
         end
         else begin
-            o_data <= memory[shifted_addr]; // Output the data at the address
+            o_data_reg <= memory[shifted_addr]; // Output the data at the address
         end
     end
 
